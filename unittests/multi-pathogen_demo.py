ae27d901b5499a430f9bb056d116367926fe5db0
Philippe Teichmann <Philippe@teichmann@gmail.com>
2023-06-30 19:52:44 -0400

    demo script update

--- unittests/multi-pathogen_demo.py
+++ unittests/multi-pathogen_demo.py
@@ -11,20 +11,19 @@ covid = inf.SARS_COV_2(20)
 
 #PATHOGEN 2: A HIGH DEATH RATE PATHOGEN
 MyNewPathogen = inf.Pathogen(5, "DeadlyPathogen") #You can use Pathogen as the starting base pathogen, or any pre-programmed pathogen such as SARS-CoV-2, and then modify its parameters
- 
-beta_dist    = dict(dist='neg_binomial', par1=1.0, par2=0.2, step=0.05) 
-beta         = 0.01      
+  
+beta         = 0.011
 prognoses = dict(
 age_cutoffs   = np.array([0]),
-sus_ORs       = np.array([1.00]),
-trans_ORs     = np.array([1.00]),
+sus_ORs       = np.array([0.5]),
+trans_ORs     = np.array([.5]),
 symp_probs    = np.array([0.75]),
 comorbidities = np.array([1.00]),
 severe_probs  = np.array([0.75]),
 crit_probs    = np.array([0.75]),
 death_probs   = np.array([0.85]))
  
-MyNewPathogen.configure_transmission(beta_dist = beta_dist, beta = beta) 
+MyNewPathogen.configure_transmission(beta = beta) 
 MyNewPathogen.configure_prognoses(prognoses, False)
    
 MyNewPathogen.add_custom_variant(label = "custom_variant1", days = 5, n_imports = 10, rel_beta = 1, rel_death_prob = 2.0)
@@ -41,6 +40,7 @@ custom_cross_immunity = dict(
             custom_variant1  = 0.92))
 
 MyNewPathogen.configure_variant_cross_immunity(custom_cross_immunity)
+MyNewPathogen.configure_generalized_immunity(0.6, 0.9, 350, 5)
 
 sim = inf.Sim(pop_size=pop_size, n_days=n_days, pathogens = [covid, MyNewPathogen])  
 sim.run()
